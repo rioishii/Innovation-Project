c5a6de911502800da8d276846a12a952e7f06d92
rioishii <rioishii@uw.edu>
2017-12-02 14:04:47 -0800

    My change

---
+++ Script/ui.R
@@ -0,0 +1,31 @@
+library(shiny)
+
+ui <- navbarPage("Earthquake Data 2017",
+           tabPanel("Home",
+                    sidebarLayout(
+                      sidebarPanel(
+                      
+                      ),
+                      mainPanel(
+                        
+                      )
+                    )
+           ),
+           tabPanel("Frequency of Magnitudes",
+                    sidebarLayout(
+                      sidebarPanel(
+                        
+                      ),
+                      mainPanel(
+                        
+                      )
+                    )
+           )
+           
+      )
+ 
+
+
+shinyUI(ui)
+
+
