8c1755ac1e4a5e0e11f90c14bcd2c619fc4fa009
Tanner Minh Le <TannerLe@uw.edu>
2017-12-02 14:02:50 -0800

    Summarize and Server.R
    Summarize/Server.R changes

---
+++ Summarize.R
@@ -0,0 +1,17 @@
+library(dplyr)
+
+info_function <- function(dataset) {
+  ret <- list()
+  #amount of earthquakes in the month
+  ret$occurances <- nrow(dataset)
+  #average depth
+  ret$avg_depth <- mean(dataset[,"depth"])
+  #finding the most common location
+  ret$avg_locaLat <- mean(dataset[, "latitude"])
+  ret$avg_locaLong <- mean(dataset[, "longitude"])
+  ret$common_loca <- tail(names(sort(table(dataset$place))), 1)
+  #finding the most average mag strength as well as the strongest one
+  ret$avg_mag <- mean(dataset[,"mag"], na.rm = TRUE)
+  ret$max_mag <- max(dataset[,"mag"], na.rm = TRUE)
+  return(ret)
+}
