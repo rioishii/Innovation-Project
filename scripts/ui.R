496bb3e6411b2400cb79e070d3e713000e96fbcd
rioishii <rioishii@uw.edu>
2017-12-03 16:22:16 -0800

    Created home page

--- scripts/ui.R
+++ scripts/ui.R
@@ -5,7 +5,37 @@ ui <- navbarPage(theme = shinytheme("slate"),"Earthquake Data 2017",
            tabPanel("Home",
                     sidebarLayout(
                       sidebarPanel(
-                      
+                        h1("Team Innovation Project"),
+                        p("Created by", strong("Rio Ishii, Tanner Le, Duke Fu, Ying Jiang")),
+                        br(),
+                        h2("About the Dataset"),
+                        p("This project's dataset contains North America's earthquake history 
+                          between Oct 22, 2017 and Nov 11, 2017. It is collected by USGS(United States Geographical Survey), 
+                          a scientific agency of US government."),
+                        p("The dataset can be found through this", a("earthquake.sgs.gov", href = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php"), "link."),
+                        h2("Our Analysis"),
+                        p("During our project, we sought to answer three questions:"),
+                        p("1. What does the distribution of earthquake magnitude look like?"),
+                        p("2. Which region experienced most frequent earthquakes?"),
+                        p("3. What does the distribution of earthquake depth look like?"),
+                        br(),
+                        p("To answer these questions, we focused on these key variables:"),
+                        p("1. Amount of earthquakes in the month: 9209"),
+                        p("2. Average of depth of earthquakes: 22.48"),
+                        p("3. Average latitude: 41.12"),
+                        p("4. Average longitude: -114.17"),
+                        p("5. Location with the most earthquakes: 14km ESE of Mammoth Lakes, California"),
+                        p("6. Most average magnitude strength: 1.51"),
+                        p("7. Strongest magnitude strength: 7.30"),
+                        h2("Technical Description"),
+                        p("This project is done by", strong("R Language"), "working in", strong("shiny app(Rstudio)"), 
+                          "with the following packages:"),
+                        p("- dplyr"),
+                        p("- ggplot2"),
+                        p("- shiny"),
+                        p("- shinythemes"),
+                        p("- ggmap"),
+                        width = 15
                       ),
                       mainPanel(
                         
